a4f8f083b80792b78d22c2663eef7acd506517a9
Hridoy <rkhridoy68@gmail.com>
2021-09-30 15:07:21 +0600

    rating functionality added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -16,13 +16,12 @@ const Product = (props) => {
             <div className="card-body p-4">
               <h5 className="card-title">{name}</h5>
               <p className="card-text">Price: ${price}</p>
-              <p className="card-text">Review: </p>
-              <Rating 
+              <p className="card-text">Rating: <Rating 
                   initialRating={star}
                   emptySymbol="far fa-star text-warning"
                   fullSymbol="fas fa-star text-warning"
                   readonly
-              />
+              /></p>
               <p className="card-text">By: {seller}</p>
               <p className="card-text">
                 <small className="text-muted">only {stock} left in stock-order soon</small>
